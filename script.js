4a0dab544fad5abb93856dcab9f64a8ddcfa63f1
Tara Solbrig <tsolbrig87@gmail.com>
2020-01-02 18:02:00 -0500

    tested API, had to get CORS but it works now

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // init commit
 
+const deckUrl = 'https://rws-cards-api.herokuapp.com/api/v1/cards'
+
+const reverseDeckUrl = 'https://rws-cards-api.herokuapp.com/api/v1/cards/search?meaning=difficult'
+
+let cityInput = document.querySelector('#city')
+let citySubmit = document.querySelector('#getTemp')
+
+async function showCards() {
+    try {
+        let getCards = await axios.get(deckUrl)
+
+        console.log(getCards)
+    } catch(err) {
+        console.log(`Oops! Error occurred! ${err}`)
+    }
+
+}
+
+showCards()
